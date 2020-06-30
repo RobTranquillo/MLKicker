7a46b46bbb67652a2a26be42a6e761f5961e8114
Robert Wartenberg <rob.tranquillo@gmx.de>
2020-06-30 14:10:16 +0200

    slow down reward

--- Assets/PlayerAgent.cs
+++ Assets/PlayerAgent.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
+using System;
+using UnityEngine.UI;
 
 public class PlayerAgent : Agent
 {
@@ -14,6 +16,7 @@ public class PlayerAgent : Agent
     public GameObject ball;
     private Rigidbody rb_ball;
     private Transform tr_ball;
+    private SphereCollider cl_ball;
 
     public Transform target;
     public Transform keepGoal;
@@ -24,14 +27,21 @@ public class PlayerAgent : Agent
     public float ballRestTimeout = 3f;
     private float ballRestBegin = 0;
     public float shootPositionRange = 4;
+    private float lastVelocity = 0f;
+    private Collides ballCollides;
+    
+    [Header("Debug Output")]
+    public TMPro.TMP_Text cumulativeReward;    
 
     private void Start()
     {
         rb_ball = ball.GetComponent<Rigidbody>();
         tr_ball = ball.GetComponent<Transform>();
         tr_pole = pole.GetComponent<Transform>();
+        ballCollides = ball.GetComponent<Collides>();
         pole_position = tr_pole.position;
         pole_rotation = tr_pole.rotation;
+        shootPositionRange = shootPositionRange/2;
         KickInTheBall();
     }
 
@@ -47,15 +57,12 @@ public class PlayerAgent : Agent
         rb_ball.angularVelocity = Vector3.zero;
         rb_ball.velocity = Vector3.zero;
 
-        float xPos = Random.value * shootPositionRange;
+        float xPos = UnityEngine.Random.Range(-shootPositionRange, shootPositionRange);
         tr_ball.localPosition = new Vector3(xPos, 0.4f, -7.45f);
 
-        // Actions, size = 2
-        Vector3 controlSignal = Vector3.zero;
-        controlSignal.x = -xPos * (kickPower / 4);
-        controlSignal.z = kickPower;
-        // Debug.Log("controlSignal " + controlSignal);
-        rb_ball.AddForce(controlSignal);
+        Vector3 controlSignal = keepGoal.position - tr_ball.position;
+        controlSignal.Normalize();
+        rb_ball.AddForce(controlSignal * kickPower);
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -76,11 +83,10 @@ public class PlayerAgent : Agent
     {
         // Actions, size = 1 //weil nur Drehung der einen Achse, würde ich sagen.
         Vector3 controlSignal = Vector3.zero;
-        controlSignal.x = vectorAction[0];
+        controlSignal.x = vectorAction[0] * 200;
         pole.AddTorque(controlSignal);
 
-        // Rewards
-
+        // sudden deaths
         if (BallOut())
         {
             Debug.Log("Out!");
@@ -92,23 +98,56 @@ public class PlayerAgent : Agent
             EndEpisode();
         }
 
-        // Reached target
-        float distanceToTarget = Vector3.Distance(tr_ball.localPosition, target.localPosition);
-        if (distanceToTarget < 1.42f)
+        // Rewards
+        if (ballCollides.touchedHomeZone)
         {
-            Debug.Log("Toooor");
+            // Debug.Log("Strafraum");
+            SetReward(-0.3f);
+            ballCollides.touchedHomeZone = false;
+        }
+
+        if (SlowDownTheBall())
+        {
+            SetReward(0.3f);
+        }
+
+        // Reached target
+        if (ballCollides.touchedTarget)
+        {
+            // Debug.Log("Toooor");
             SetReward(1.0f);
+            ballCollides.ResetValues();
             EndEpisode();
         }
 
         // Tor kassiert
-        float distanceToKeepGoal = Vector3.Distance(tr_ball.localPosition, keepGoal.localPosition);
-        if (distanceToKeepGoal < 1.42f)
+        if (ballCollides.touchedSelfGoal)
         {
-            Debug.Log("EIGENTOR");
+            // Debug.Log("EIGENTOR");
             SetReward(-1.0f);
+            ballCollides.ResetValues();
             EndEpisode();
         }
+
+        cumulativeReward.text = GetCumulativeReward().ToString("R");
+    }
+
+    //wenn jetzt die Beschleunigung viel kleiner ist als beim letzten Mal gibts reward
+    private bool SlowDownTheBall()
+    {
+        if( ! ballCollides.touchedPlayer )
+        {
+            return false;
+        }
+        float thisVelocity = Mathf.Abs(rb_ball.velocity.z);
+        if (thisVelocity < lastVelocity)
+        {
+            // Debug.Log("SlowDownBall");
+            lastVelocity = thisVelocity;
+            return true;
+        }
+        lastVelocity = thisVelocity;
+        return false;
     }
 
     private bool BallOut()
