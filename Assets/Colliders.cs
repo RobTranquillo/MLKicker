d9d8d1092f0b67b32add5534e899748efea789d6
Robert Wartenberg <rob.tranquillo@gmx.de>
2020-07-03 09:54:08 +0200

    add touch flag, reset values

--- Assets/Colliders.cs
+++ Assets/Colliders.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collides : MonoBehaviour
+public class Colliders : MonoBehaviour
 {
     [HideInInspector]
     public bool touchedPlayer = false;
+    [HideInInspector]
+    public bool touchedPole = false;
     [HideInInspector]
     public bool touchedTarget = false;
     [HideInInspector]
@@ -15,6 +17,7 @@ public class Collides : MonoBehaviour
     [HideInInspector]
     public bool touchedTargetZone = false;
 
+    public BoxCollider pole;
     public BoxCollider polePlayer1;
     public BoxCollider polePlayer2;
     public BoxCollider polePlayer3;
@@ -26,6 +29,7 @@ public class Collides : MonoBehaviour
     public void ResetValues()
     {
         touchedPlayer = false;
+        touchedPole = false;
         touchedTarget = false;
         touchedSelfGoal = false;
         touchedHomeZone = false;
@@ -47,13 +51,18 @@ public class Collides : MonoBehaviour
         touchedTarget = collision.collider == target;
         touchedSelfGoal = collision.collider == selfGoal;
         touchedPlayer = (collision.collider == polePlayer1 || collision.collider == polePlayer2 || collision.collider == polePlayer3);
+        touchedPole = collision.collider == pole;
     }
-    void OnCollisionExit(Collision collision)
+    void OnCollisionExit()
     {
-        touchedHomeZone = collision.collider == homeZone;
-        touchedTargetZone = collision.collider == targetZone;
-        touchedPlayer = (collision.collider == polePlayer1 || collision.collider == polePlayer2 || collision.collider == polePlayer3);
-        touchedTarget = collision.collider == target;
-        touchedSelfGoal = collision.collider == selfGoal;
+        ResetValues();
+        // würde sagen, das wird nicht benötigt. einfach beim verlassen alle states zurücksetzen
+        // das ist dann gefärlich bis falsch, wenn mehrere Objekte zeitüberlagernd collidieren können. zB. Player & HomeZone 
+        // touchedHomeZone = collision.collider == homeZone;
+        // touchedTargetZone = collision.collider == targetZone;
+        // touchedPlayer = (collision.collider == polePlayer1 || collision.collider == polePlayer2 || collision.collider == polePlayer3);
+        // touchedPole = collision.collider == pole;
+        // touchedTarget = collision.collider == target;
+        // touchedSelfGoal = collision.collider == selfGoal;
     }
 }
