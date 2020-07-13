e204c8394a3cbb6492fc329e443e4d99e9da601e
Robert Wartenberg <rob.tranquillo@gmx.de>
2020-07-13 12:16:02 +0200

    add gampad testing scene

---
+++ Assets/GamePadInput.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[Flags]
+public enum Hand
+{
+    None = 0b_0000_0000,    // 0
+    Defence = 0b_0000_0001, // 1
+    Offense = 0b_0000_0010  // 2
+}    
+
+
+
+public class GamePadInput : MonoBehaviour
+{
+    public GameObject[] poleBars = new GameObject[4];
+    public Material selectedMaterial;
+    public Material unselectedMaterial;
+
+    public float poleSwitchDelay = 0.9f; //Seconds delay between switching the poles
+    
+    private Hand _hand;
+    private Gamepad _gamePad;
+    
+    float _nextSwitch;
+    
+    void Start()
+    {
+        _gamePad = Gamepad.current;
+        _nextSwitch = Time.time;
+        
+        _hand = Hand.Offense;
+        SwitchHand(Hand.Defence);
+    }
+
+    void Update()
+    {
+        if (Time.time < _nextSwitch)
+            return;
+        
+        var left = _gamePad.leftTrigger.ReadValue();
+        var right = _gamePad.rightTrigger.ReadValue();
+        if (left <= 0.2f && right <= 0.2f || left == 0 && right == 0)
+            return;
+            
+        SwitchHandLeft(left);
+        SwitchHandRight(right);
+        _nextSwitch = Time.time + poleSwitchDelay;
+    }
+
+    private void SwitchHandLeft(float val)
+    {
+        if (val >= 0.2f && _hand == Hand.Offense)
+            SwitchHand(Hand.Defence);
+    }
+    private void SwitchHandRight(float val)
+    {
+        if (val >= 0.2f && _hand == Hand.Defence)
+            SwitchHand(Hand.Offense);
+    }
+    private void SwitchHand(Hand newHand)
+    {
+        Debug.Log(("Handwechsel: " + newHand));
+        _hand = newHand;
+
+        if (_hand == Hand.Defence)
+        {
+            poleBars[0].GetComponent<Usage>().state = true;
+            poleBars[1].GetComponent<Usage>().state = true;
+            poleBars[2].GetComponent<Usage>().state = false;
+            poleBars[3].GetComponent<Usage>().state = false;
+        }
+        else
+        {
+            poleBars[0].GetComponent<Usage>().state = false;
+            poleBars[1].GetComponent<Usage>().state = false;
+            poleBars[2].GetComponent<Usage>().state = true;
+            poleBars[3].GetComponent<Usage>().state = true;
+        }
+    }
+}
---
+++ Assets/Usage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Usage : MonoBehaviour
+{
+    public GameObject[] objects = new GameObject[2];
+    public Material selectedMaterial;
+    public Material unselectedMaterial;
+    public bool state;
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Material thisMat = state == true ? selectedMaterial : unselectedMaterial;
+        foreach (var go in objects)
+            go.GetComponent<Renderer>().material = thisMat;
+    }
+}
