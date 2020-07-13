87c0ca12551a801447ff19eeeb5825e4eea4ca54
LAPTOP-L8PT5PRR\VRBits <luise@vr-bits.de>
2020-07-13 12:33:22 +0200

    Kicker Test Szene ohne Input

---
+++ Assets/KickerContent/Scripts/CountDown.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    public float timeLeft = 15;
+    public TorTrigger TorRed;
+    public TorTrigger TorBlue;
+    public GameObject ball;
+    public ParticleSystem end;
+    Vector3 startPosBall;
+
+    public Text time;
+
+    void Start()
+    {
+        startPosBall = ball.transform.position;
+    }
+
+    void Update()
+    {
+
+        timeLeft -= Time.deltaTime;
+        time.text = "Time Left:" + Mathf.Round(timeLeft);
+        if (timeLeft < 0)
+        {
+            if (end)
+                end.Play();
+            timeLeft = 15;
+            TorRed.ResetScore();
+            TorBlue.ResetScore();
+            ball.transform.position = startPosBall;
+
+
+        }
+    }
+}
---
+++ Assets/KickerContent/Scripts/TorTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TorTrigger : MonoBehaviour
+{
+	public Text scoreText;
+	int score;
+    public ParticleSystem winPS;
+	public string compareTag = "Ball";
+    public GameObject ball;
+    Vector3 startPosBall;
+
+    public void Start()
+    {
+        startPosBall = ball.transform.position;
+    }
+    private void OnTriggerEnter(Collider other) {
+		
+		if (other.CompareTag(compareTag)) {
+            ball.transform.position = startPosBall;
+            IncreaseScore();
+            OnTor();
+		}
+	}
+
+	void OnTor() {
+		if (winPS)
+			winPS.Play();
+	}
+    public void IncreaseScore() {
+        SetScore(++score);
+    }
+    public void ResetScore() {
+        SetScore(0);
+    }
+
+    private void SetScore(int v) {
+        score = v;
+        if (scoreText)
+            scoreText.text = (score).ToString();
+    }
+
+    
+}
