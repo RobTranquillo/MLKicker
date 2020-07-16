0e940947e95888c01f931c69eb85d9a439f1fddc
Robert Wartenberg <rob.tranquillo@gmx.de>
2020-07-16 14:21:37 +0200

    multiball feature

---
+++ Assets/_Items/MultiBallEffect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiBallEffect : MonoBehaviour
+{
+    public GameObject Ball;
+    public ParticleSystem Collect;
+    [Range(0, 20)]
+    public int count = 4;
+    [Range(0, 20)]
+    public int livingTime = 4;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            GetComponent<Collider>().enabled = false;
+            StartCoroutine(ActivateCollider());
+            
+            if (Collect)
+                Collect.Play();
+            SpawnBalls();
+        }
+    }
+
+    private void SpawnBalls()
+    {
+        for (int i = 0; i < count; i++)
+            Destroy(
+                Instantiate(Ball, transform.position, Quaternion.identity), 
+                livingTime);
+    }
+
+    IEnumerator ActivateCollider()
+    {
+        yield return new WaitForSeconds(livingTime);
+        GetComponent<Collider>().enabled = true;
+    }
+}
