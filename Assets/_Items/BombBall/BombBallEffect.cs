7867a8a0e6dae448b4840db01cd8f33d9e3110c1
Luise Kneitschel <luise@vr-bits.de>
2020-07-20 12:13:13 +0200

    BombBall Item angelegt, bisher ohne funktion, Spielfeld Collider implementiert

---
+++ Assets/_Items/BombBall/BombBallEffect.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class BombBallEffect : MonoBehaviour
+{
+    public ItemEffectBombBall settings;
+    private GameObject _ball;
+
+    public void Start()
+    {
+        _ball = GameObject.FindWithTag("Ball");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _ball)
+        {
+            //DeactivateItem();
+            if (settings.particleEffect)
+            {
+                var ps = Instantiate(settings.particleEffect, this.transform).GetComponent<ParticleSystem>();
+                ps.Play();
+            }
+            //SpawnBalls();
+        }
+    }
+
+  /* private void SpawnBalls()
+    {
+        for (int i = 0; i < settings.count; i++)
+            Destroy(
+                Instantiate(_ball, transform.position, Quaternion.identity), 
+                settings.livingTime);
+    }
+
+    private void DeactivateItem()
+    {
+        GetComponent<Collider>().enabled = false;
+        GetComponent<Renderer>().enabled = false;
+        StartCoroutine(ActivateItem());
+    }
+    IEnumerator ActivateItem()
+    {
+        yield return new WaitForSeconds(settings.livingTime);
+        GetComponent<Collider>().enabled = true;
+        GetComponent<Renderer>().enabled = true;
+    }*/
+}
---
+++ Assets/_Script/Item/ItemEffectBombBall.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BombBall_EffectSettings", menuName = "KickerItem/Create BombBall effect settings")]
+public class ItemEffectBombBall : ScriptableObject
+{
+    [Header("General Settings")]
+    public GameObject particleEffect;
+    [Header("Effect Settings")]
+    [Range(0, 20)]
+    public int count = 4;
+    [Range(0, 20)]
+    public int livingTime = 4;
+}
